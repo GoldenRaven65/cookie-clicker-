184ca2eac31274a3b3456dd960c7fd9ed555b9f5
Ben Prinssen <benprinssen@gmail.com>
2025-02-14 08:51:24 +0100

    second
    third
    
    fourth

--- script.js
+++ script.js
@@ -5,27 +5,37 @@ class CookieClicker {
         this.upgradeCost = 50;
         this.autoUpgradeCost = 10;
         this.superUpgradeCost = 100;
+        this.dansupgradeCost = 300;
         this.cookiesPerSecond = 0;
+        this.UpgradeBought = 0;
         this.cookieElement = document.getElementById("cookie");
         this.countElement = document.getElementById("cookie-count");
         this.upgradeButton = document.getElementById("upgrade");
         this.autoUpgradeButton = document.getElementById("auto-upgrade");
         this.superUpgradeButton = document.getElementById("super-upgrade");
+        this.dansupgradeButton = document.getElementById("dans-upgrade")
         this.rewardVideo = document.getElementById("reward-video");
         
         this.cookieElement.addEventListener("click", () => this.clickCookie());
         this.upgradeButton.addEventListener("click", () => this.buyUpgrade());
         this.autoUpgradeButton.addEventListener("click", () => this.buyAutoUpgrade());
         this.superUpgradeButton.addEventListener("click", () => this.buySuperUpgrade());
-        
+        this.dansupgradeButton.addEventListener("click", () => this.buydansupgrade())
+       
+        var tempcookie = localStorage.getItem("cookies");
+        if(tempcookie == "undefind"){
+        localStorage.setItem("cookies", this.cookies)
+        }
+        else{
+            this.cookies = Number(tempcookie)
+        }
+        console.log(this.cookies);
         setInterval(() => this.generateAutoCookies(), 1000);
     }
-
     clickCookie() {
         this.cookies += this.cookiesPerClick;
         this.updateUI();
     }
-
     buyUpgrade() {
         if (this.cookies >= this.upgradeCost) {
             this.cookies -= this.upgradeCost;
@@ -35,7 +45,6 @@ class CookieClicker {
             this.updateUI();
         }
     }
-
     buyAutoUpgrade() {
         if (this.cookies >= this.autoUpgradeCost) {
             this.cookies -= this.autoUpgradeCost;
@@ -45,27 +54,47 @@ class CookieClicker {
             this.updateUI();
         }
     }
-
     buySuperUpgrade() {
         if (this.cookies >= this.superUpgradeCost) {
             this.cookies -= this.superUpgradeCost;
             this.cookiesPerSecond += 1;
+            this.UpgradeBought += 1;
             this.superUpgradeCost = Math.floor(this.superUpgradeCost * 1.15);
             this.superUpgradeButton.textContent = `Super Auto Cookie (+1/sec) [Cost: ${this.superUpgradeCost}]`;
             this.rewardVideo.style.display = "block";
             this.rewardVideo.play();
             this.updateUI();
         }
+        if(this.UpgradeBought = 1){
+            document.getElementById("super-upgrade").remove();
+        }
     }
-
+    buydansupgrade(){
+    if(this.cookies >= this.dansupgradeCost){
+        this.cookies -= this.dansupgradeCost;
+        this.cookiesPerSecond += 8;
+        this.UpgradeBought += 1;
+        this.dansupgradeCost = Math.floor(this.dansupgradeCost * 1.15)
+        this.dansupgradeButton.textContent = `Super Auto Cookie (+1/sec) [Cost: ${this.dansupgradeCost}]`;
+        // de video aan kondigen andere id en video er in zetten
+        // this.rewardVideo.style.display = "block";
+        // this.rewardVideo.play();
+        this.updateUI();
+      }
+      if(this.UpgradeBought <= 1 ){
+        document.getElementById("dans-upgrade").style.visibility = "hidden";
+    }
+      if(this.UpgradeBought = 2){
+        document.getElementById("dans-upgrade").remove();
+      }
+    }
     generateAutoCookies() {
         this.cookies += this.cookiesPerSecond;
         this.updateUI();
     }
-
     updateUI() {
         this.countElement.textContent = this.cookies;
+        localStorage.setItem("cookies", this.cookies)
     }
 }
-
 new CookieClicker();
